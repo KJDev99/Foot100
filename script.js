ef432ad3195f54d6bb6f013cb9aa939171243147
KJ_dev <jamshidqayimov0399@gmail.com>
2021-05-27 09:51:55 +0500

    uz auto

--- script.js
+++ script.js
@@ -1,39 +1,75 @@
 const products = {
-  plainBurger: {
-    name: 'GAMBURGER',
+  matiz: {
+    name: 'MATIZ',
+    price: 5000,
+    amount: 0,
+    get Sum() {
+      return this.price * this.amount
+    },
+  },
+  spark: {
+    name: 'SPARK',
+    price: 7500,
+    amount: 0,
+    get Sum() {
+      return this.price * this.amount
+    },
+  },
+  nexia: {
+    name: 'NEXIA',
+    price: 8500,
+    amount: 0,
+    get Sum() {
+      return this.price * this.amount
+    },
+  },
+  ravon: {
+    name: 'RAVON',
     price: 10000,
-    kcal: 200,
     amount: 0,
     get Sum() {
       return this.price * this.amount
     },
-    get Kcal() {
-      return this.kcal * this.amount
-    }
   },
-  freshBurger: {
-    name: 'GAMBURGER FRESH',
-    price: 20500,
-    kcal: 280,
+  lacetti: {
+    name: 'LACETTI',
+    price: 13500,
     amount: 0,
     get Sum() {
       return this.price * this.amount
     },
-    get Kcal() {
-      return this.kcal * this.amount
-    }
   },
-  freshCombo: {
-    name: 'FRESH COMBO',
-    price: 31900,
-    kcal: 350,
+  cobalt: {
+    name: 'COBALT',
+    price: 15000,
     amount: 0,
     get Sum() {
       return this.price * this.amount
     },
-    get Kcal() {
-      return this.kcal * this.amount
-    }
+  },
+  gentra: {
+    name: 'GENTRA',
+    price: 17000,
+    amount: 0,
+    get Sum() {
+      return this.price * this.amount
+    },
+  },
+  orlando: {
+    name: 'ORLANDO',
+    price: 20000,
+    amount: 0,
+    get Sum() {
+      return this.price * this.amount
+    },
+  },
+  malibu: {
+    name: 'MALIBU',
+    price: 25000,
+    amount: 0,
+    get Sum() {
+      return this.price * this.amount
+    },
   },
 }
 
@@ -57,7 +93,6 @@ function buy(btn) {
     parentId = parent.getAttribute('id'),
     productNum = parent.querySelector('.main__product-num'),
     productPrice = parent.querySelector('.main__product-price span'),
-    productKcal = parent.querySelector('.main__product-kcall span'),
     btnSymbol = btn.getAttribute('data-symbol')
   if (btnSymbol === '+' && products[parentId].amount < 10) {
     var nums = products[parentId].amount++
@@ -67,12 +102,10 @@ function buy(btn) {
   }
   productNum.innerHTML = products[parentId].amount
   productPrice.innerHTML = products[parentId].Sum
-  productKcal.innerHTML = products[parentId].Kcal
 }
 let arrayProducts = [];
 totalName = '';
 totalPrice = 0;
-totalKcal = 0;
 totalNums = 0;
 addCart.addEventListener('click', () => {
   for (const key in products) {
@@ -81,12 +114,10 @@ addCart.addEventListener('click', () => {
   }
 
   arrayProducts.forEach(el => {
-    totalName += `\n${el.name}\n`
+    totalName += `\n${el.name} - ${el.amount}\n`
     totalPrice += el.Sum
-    totalKcal += el.Kcal
-    totalNums += el.amount
   })
-  receiptOut.innerHTML = `Purchased: \n${totalName}\nCalories: ${totalKcal}\nToal Prices: ${totalPrice} sum`
+  receiptOut.innerHTML = `Purchased: \n${totalName}\nToal Prices: ${totalPrice} $`
   receipt.style.display = 'flex'
   setTimeout(() => {
     receipt.style.opacity = 1
@@ -99,7 +130,7 @@ receiptBtn.addEventListener('click', () => location.reload())
 
 const info = document.querySelectorAll('.main__product-info');
       view = document.querySelector('.view');
-      viewImg = document.querySelector('img');
+      viewImg = document.querySelector('.view__img');
       viewClose = document.querySelector('.view__close');
 
 var val = "";      
@@ -119,27 +150,51 @@ info.forEach(el => el.addEventListener(val, e=>{
     .getAttribute('src')
   viewImg.setAttribute('src', img)
   view.classList.add('active')
+  console.log(img);
 }))
 
 viewClose.addEventListener('click', ()=>view.classList.remove('active'))
 
 var timer = document.querySelector('.header__timer-extra');
+var timerText = document.querySelector('.header__timer-text');
 
 let time = 50
 
 const count = (i = 0) => {
   timer.innerHTML = i
-  if (timer.innerHTML === '100') return;
-  if (timer.innerHTML === '50') time = 100;
-  if (timer.innerHTML === '75') time = 150;
-  if (timer.innerHTML === '90') time = 200;
-  if (timer.innerHTML === '95') time = 250;
-  if (timer.innerHTML === '99') time = 500;
+  if (timer.innerHTML === '100') {
+    timerText.innerHTML = 'STOP'
+    timer.style.color = 'red'
+    timerText.style.color = 'red'
+    return
+  };
+  if (timer.innerHTML === '50') {
+    time = 100
+    timer.style.color = 'green'
+    timerText.style.color = 'green'
+  };
+  if (timer.innerHTML === '75') {
+    time = 150
+    timer.style.color = 'yellow'
+    timerText.style.color = 'yellow'
+  };
+  if (timer.innerHTML === '90') {
+    time = 200
+    timer.style.color = 'rgba(209, 107, 23, 0.835)'
+    timerText.style.color = 'rgba(209, 107, 23, 0.835)'
+  };
+  if (timer.innerHTML === '95') {
+    time = 250
+    timer.style.color = 'rgba(255, 99, 99, 0.835)'
+    timerText.style.color = 'rgba(255, 99, 99, 0.835)'
+  };
+  if (timer.innerHTML === '100') {
+    time = 500
+  };
   i++
   setTimeout(() => {
     count(i)
   }, time);
 }
 count()
-
 
